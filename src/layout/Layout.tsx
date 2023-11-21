5fa8b341632b9cd8577ba2d695ecaf2704e279ce
gsemir0418 <845217811@qq.com>
2023-11-21 17:24:22 +0800

    feat: Layout

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Sider } from '@/components/Sider'
+import { Header } from '@/components/Header'
 
 interface Props {}
 export const Layout: React.FC<Props> = () => {
   return (
     <div className=" bg-black h-[100vh] w-[100vw] relative flex flex-col">
-      <div className=" bg-red-200 w-full h-20 flex-shrink-0">Header</div>
+      <Header />
       <div className=" flex flex-row flex-1">
         <Sider />
         <div className=" bg-slate-200 flex-1">
